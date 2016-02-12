5bc5207d407866b87cf617c39dffa0d54c3ae189
Scotty Runyan <runyans7@gmail.com>
2016-02-11 22:11:25 -0500

    User model specs green

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    happiness > nausea ? "happy" : "sad"
+  end
 end
